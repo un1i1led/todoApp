731fc62a798ee4b5effe6e9d13785eded259ea44
gilberto <gsqwow@gmail.com>
2022-09-12 09:56:37 -0600

    Add starter modules

---
+++ src/modules/todoItem.js
@@ -0,0 +1,14 @@
+let createItem = (title, description, dueDate, priority) => {
+    let itemId = Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+
+    return {
+        title,
+        description,
+        dueDate,
+        priority,
+        itemId
+    }
+}
+
