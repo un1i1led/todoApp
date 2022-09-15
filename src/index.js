5f5f74cac53f5f4fe55d2c583785c6a0f22d11cf
gilberto <gsqwow@gmail.com>
2022-09-15 14:05:26 -0600

    Add makeTodo function and listener

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+import {makeTodoItem} from '../src/modules/UI';
+
 const addBtn = document.querySelector('#add-todo-btn');
 const cancel = document.querySelector('#cancel-btn');
 const form = document.getElementById('form-pop');
+const makeTodoBtn = document.getElementById('add-form-btn');
+
 addBtn.addEventListener('click', () => {
     form.style.display = "block";
 })
 
 cancel.addEventListener('click', () => {
     form.style.display = "none";
+})
+
+makeTodoBtn.addEventListener('click', function () {
+    makeTodoItem();
 })
 
