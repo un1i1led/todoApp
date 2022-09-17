97085b052a422895c064be45149d3688c454d945
gilberto <gsqwow@gmail.com>
2022-09-16 18:27:30 -0600

    Add listeners to createProject and to tab switch to Inbox

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import {makeTodoItem} from '../src/modules/UI';
+import {createProject} from '../src/modules/projects';
 
 const addBtn = document.querySelector('#add-todo-btn');
 const cancel = document.querySelector('#cancel-btn');
 const form = document.getElementById('form-pop');
 const makeTodoBtn = document.getElementById('add-form-btn');
+const todosInbox = document.querySelector('.todos-inbox');
+
 
 addBtn.addEventListener('click', () => {
     form.style.display = "block";
@@ -16,4 +19,17 @@ cancel.addEventListener('click', () => {
 makeTodoBtn.addEventListener('click', function () {
     makeTodoItem();
 })
+
+document.querySelector('.projects').addEventListener('click', function () {
+    const name = window.prompt("Enter name of project: ");
+    createProject(name);
+})
+
+document.querySelector('#inbox-p').addEventListener('click', function() {
+    const todoSection = document.querySelector('.todoSection');
+    const currentDiv = todoSection.firstElementChild;
+    currentDiv.remove();
+    todoSection.insertBefore(todosInbox, todoSection.firstChild);
+})
+
 
