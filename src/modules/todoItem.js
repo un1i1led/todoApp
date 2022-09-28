4da778249a066d5fa76bf86c48ef6de1dfcdfbe3
gilberto <gsqwow@gmail.com>
2022-09-28 10:43:10 -0600

    Remove project attribute

--- src/modules/todoItem.js
+++ src/modules/todoItem.js
@@ -1,15 +1,13 @@
-let createItem = (title, description, dueDate, priority) => {
+const createItem = (title, description, dueDate, priority) => {
     let itemId = Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
-    let project = document.querySelector('.active').textContent;
 
     return {
         title,
         description,
         dueDate,
         priority,
-        project,
         itemId
     }
 }
