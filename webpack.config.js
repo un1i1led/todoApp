a25ec7f550869a01332022fa527b6d86a9d44289
gilberto <gsqwow@gmail.com>
2022-10-01 13:13:11 -0600

    Add cs loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test:/\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
